08f71f4c19be242481f39abb418fcaecdae9691e
Crislaine Soares <crislaines@dimed.com.br>
2022-05-06 08:02:17 -0300

    Edit secret google

--- src/pages/Mapa.js
+++ src/pages/Mapa.js
@@ -90,7 +90,7 @@ export default function Mapa() {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       <MapWrapped
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCFAKJ1oamqx7jcHpIEJM4DJINBI_BSaSM`}
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=<google-secret>`}
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `100%` }} />}
         mapElement={<div style={{ height: `100%` }} />}
